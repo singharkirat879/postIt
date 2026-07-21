6bffcb7f07eca1036afd390ecbdb401d79ebb575
singharkirat879 <sharkirat879@gmail.com>
2026-07-21 23:19:13 +0530

    pls

--- util/cloudinary.js
+++ util/cloudinary.js
@@ -6,4 +6,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+console.log("TYPE:", typeof process.env.CLOUDINARY_API_KEY);
+console.log("LEN:", process.env.CLOUDINARY_API_KEY?.length);
+console.log("KEY:", JSON.stringify(process.env.CLOUDINARY_API_KEY));
 module.exports = cloudinary;
